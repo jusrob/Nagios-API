b7d4569669a8f201dc377db3cbd36306683d7397
Justin Roberts <justin.roberts@cdimac0020.churchill.cdi.com>
2016-05-11 17:24:15 -0400

    major update

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# Nagios-API
-REST API for configuring host on Nagios 4
+# nagios-api
+
+## API
+
+##End Points
+| Endpoint | HTTP Method | Action |
+|:--------:|:-----------:|:--------:|:-----------:|:--------:|
+| POST | /new  | Add a host to nagios |
+| DELETE | /:hostname  | Delete a host from nagios |
+
+
+### POST
+#### input (any number of services can be provided)
+##### for config help https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/4/en/objectdefinitions.html
+```
+{
+	"hostname": "test.example.com",
+	"hostgroups": "all,production,p10",
+	"ip": "1.1.1.1",
+	"service1": {
+		"check_command": "check_nrpe!check_zombie_procs",
+		"service_description": "procs in zombie",
+		"use": "generic-service"
+	},
+  "service2": {
+    "check_command": "check_nrpe!check_load",
+    "service_description": "load",
+    "use": "generic-service"
+  },
+  "service3": {
+    "check_command": "check_nrpe!check_iowait_procs",
+    "service_description": "procs in iowait",
+    "use": "generic-service"
+  },
+  "service4": {
+    "check_command": "check_nrpe!check_disk_boot",
+    "service_description": "disk utilization boot",
+    "use": "generic-service"
+  },
+  "service5": {
+    "check_command": "check_nrpe!check_disk_root",
+    "service_description": "disk utilization root",
+    "use": "generic-service"
+  },
+  "service6": {
+    "check_command": "check_nrpe!check_mem",
+    "service_description": "memory utilizationt",
+    "use": "generic-service"
+  },
+  "service7": {
+    "check_command": "check_nrpe!check_swap",
+    "service_description": "swap utilization",
+    "use": "generic-service"
+  },
+  "service8": {
+    "check_command": "check_nrpe!check_total_proc",
+    "service_description": "total procs",
+    "use": "generic-service"
+  },
+}
+```
+
+##### POST example:
+```
+curl -H "Content-Type: application/json" -X POST -d '{"hostname": "test.example.com","hostgroups": "all,production,p10","ip": "1.1.1.1","service1": {"check_command": "check_nrpe!check_zombie_procs", "service_description": "procs in zombie", "use": "generic-service"},"service2": {"check_command": "check_nrpe!check_load","service_description": "loade","use": "generic-service"}}' http://<host>:8080/new
+```
+
+### DELETE
+
+##### DELETE example:
+```
+curl -H "Content-Type: application/json" -X DELETE http://<host>:8080/test.example.com
+```
--- main.rb
+++ main.rb
@@ -15,12 +15,12 @@ end
 
 def buildHostConfig(config_parameters)
   hostConfigString = "define host {\n"\
-    "  address " + config_parameters["ip"].to_s + "\n"\
-    "  check_command check-host-alive\n"\
-    "  host_name " + config_parameters["hostname"].to_s + "\n"\
-    "  hostgroups " + config_parameters["hostgroups"].to_s + "\n"\
-    "  max_check_attemps 5\n"\
-    "  use generic-host\n"\
+    "     address " + config_parameters["ip"].to_s + "\n"\
+    "     check_command check-host-alive\n"\
+    "     host_name " + config_parameters["hostname"].to_s + "\n"\
+    "     hostgroups " + config_parameters["hostgroups"].to_s + "\n"\
+    "     max_check_attempts 5\n"\
+    "     use generic-host\n"\
   "}\n"
   return hostConfigString
 end
@@ -28,30 +28,40 @@ end
 def buildServiceConfigs(config_parameters)
   serviceConfigString = ""
   config_parameters.each do |key, value|
-    if key.start_with?("servicecmd")
-      serviceNumber = key.split("servicecmd")[1]
-      if config_parameters["serviceuse#{serviceNumber}"].to_s != ""
-        serviceUse = config_parameters["serviceuse#{serviceNumber}"]
-      else
-        serviceUse = "generic-service"
+    if key.start_with?("service")
+      serviceConfigString << "define service {\n"
+      serviceConfigString << "     host_name " + config_parameters['hostname'] + "\n"
+      value.each do |name, setting|
+        serviceConfigString << "     " + name.to_s + " " + setting.to_s + "\n"
       end
-      serviceConfigString << "define service {\n" \
-        "  check_command " + config_parameters["servicecmd#{serviceNumber}"].to_s + "\n"\
-        "  host_name " + config_parameters["hostname"].to_s + "\n"\
-        "  service_description " + config_parameters["servicedesc#{serviceNumber}"].to_s + "\n"\
-        "  use " + serviceUse.to_s + "\n"\
-        "}\n\n"
+      serviceConfigString << "}\n"
     end
   end
   return serviceConfigString
 end
 
+def checkConfig()
+  status = `nagios -v /etc/nagios/nagios.cfg`
+  return $?
+end
+
+def restartNagios()
+  status = `systemctl restart nagios`
+  return $?
+end
+
 delete '/:host' do
-  config_filepath = "#{params[:host]}.cfg"
+  config_filepath = "/etc/nagios/conf.d/#{params[:host]}.cfg"
   if File.exist?("#{config_filepath}")
     begin
       File.delete("#{config_filepath}")
-      `service nagios restart`
+      if restartNagios() == 0
+        logger.info "restart of nagios successful return 200"
+        status 200
+      else
+        logger.error "restart of nagios failed return 500"
+        status 500
+      end
     rescue => e
       logger.error "error deleting file, received exception #{e.message} return 500"
       status 500
@@ -66,7 +76,7 @@ post '/new' do
   config_parameters = JSON.parse(request.body.read)
   hostConfigString = buildHostConfig(config_parameters)
   serviceConfigString = buildServiceConfigs(config_parameters)
-  config_filepath = "#{config_parameters['hostname']}.cfg"
+  config_filepath = "/etc/nagios/conf.d/#{config_parameters['hostname']}.cfg"
   unless File.exist?("#{config_filepath}")
     begin
       config_file = File.new("#{config_filepath}", "w")
@@ -76,6 +86,20 @@ post '/new' do
     rescue => e
       logger.error "error creating file, received exception #{e.message} return 500"
       status 500
+    end
+    if checkConfig() == 0
+      logger.info "config check passed, restarting nagios services"
+      if restartNagios() == 0
+        logger.info "restart of nagios successful return 200"
+        status 200
+      else
+        logger.error "restart of nagios failed return 500"
+        status 500
+      end
+    else
+      logger.error "config check failed, did not restart nagios services return 400"
+      status 400
+      File.delete("#{config_filepath}")
     end
   else
     status 409
