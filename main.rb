ff2e55c7765dddc0c7ebe2b3b345d34fe95f00b3
jusrob <jm.roberts@outlook.com>
2016-05-20 13:41:53 -0400

    lots of refactoring

--- main.rb
+++ main.rb
@@ -6,103 +6,107 @@ require 'sinatra'
 require 'json'
 require 'logger'
 
+CONFIG_LOCATION = '/etc/nagios/conf.d/'.freeze
 
-enable :logging
-
-before do
-  logger.level = Logger::DEBUG
-end
-
-def buildHostConfig(config_parameters)
-  hostConfigString = "define host {\n"\
-    "     address " + config_parameters["ip"].to_s + "\n"\
-    "     check_command check-host-alive\n"\
-    "     host_name " + config_parameters["hostname"].to_s + "\n"\
-    "     hostgroups " + config_parameters["hostgroups"].to_s + "\n"\
-    "     max_check_attempts 5\n"\
-    "     use generic-host\n"\
-  "}\n"
-  return hostConfigString
-end
-
-def buildServiceConfigs(config_parameters)
-  serviceConfigString = ""
-  config_parameters.each do |key, value|
-    if key.start_with?("service")
-      serviceConfigString << "define service {\n"
-      serviceConfigString << "     host_name " + config_parameters['hostname'] + "\n"
-      value.each do |name, setting|
-        serviceConfigString << "     " + name.to_s + " " + setting.to_s + "\n"
-      end
-      serviceConfigString << "}\n"
+# Initialize logger
+class Mylog
+  def self.log
+    if @logger.nil?
+      @logger = Logger.new STDOUT
+      @logger.level = Logger::DEBUG
+      @logger.datetime_format = '%Y-%m-%d %H:%M:%S '
     end
+    @logger
   end
-  return serviceConfigString
 end
 
-def checkConfig()
-  status = `nagios -v /etc/nagios/nagios.cfg`
-  return $?
+def build_host_config(config_parameters)
+  "define host {\n"\
+  '     address ' + config_parameters['ip'].to_s + "\n"\
+  "     check_command check-host-alive\n"\
+  '     host_name ' + config_parameters['hostname'].to_s + "\n"\
+  '     hostgroups ' + config_parameters['hostgroups'].to_s + "\n"\
+  "     max_check_attempts 5\n"\
+  "     use generic-host\n"\
+  "}\n"
 end
 
-def restartNagios()
-  status = `systemctl restart nagios`
-  return $?
+def build_service_configs(config_parameters)
+  service_config_string = ''
+  config_parameters['services'].each do |service|
+    service_config_string << "define service {\n"
+    service_config_string << "     host_name #{config_parameters['hostname']}\n"
+    service.each do |key, value|
+      service_config_string << '     ' + key.to_s + ' ' + value.to_s + "\n"
+    end
+    service_config_string << "}\n"
+  end
+  service_config_string
+end
+
+def check_config
+  `nagios -v /etc/nagios/nagios.cfg`
+  $?
+end
+
+def restart_nagios
+  `systemctl restart nagios`
+  $?
 end
 
 delete '/:host' do
-  config_filepath = "/etc/nagios/conf.d/#{params[:host]}.cfg"
-  if File.exist?("#{config_filepath}")
+  config_filepath = "#{CONFIG_LOCATION}#{params[:host]}.cfg"
+  if File.exist?(config_filepath)
     begin
-      File.delete("#{config_filepath}")
-      if restartNagios() == 0
-        logger.info "restart of nagios successful return 200"
+      File.delete(config_filepath)
+      if restart_nagios == 0
         status 200
+        Mylog.log.info 'restart of nagios successful return 200'
       else
-        logger.error "restart of nagios failed return 500"
         status 500
+        Mylog.log.error 'restart of nagios failed return 500'
       end
     rescue => e
-      logger.error "error deleting file, received exception #{e.message} return 500"
       status 500
+      Mylog.log.error "error deleting file, received exception #{e.message} return 500"
     end
   else
     status 404
-    logger.info "config file #{config_filepath} does not exist, returning 404"
+    Mylog.log.info "config file #{config_filepath} does not exist, returning 404"
   end
 end
 
 post '/new' do
   config_parameters = JSON.parse(request.body.read)
-  hostConfigString = buildHostConfig(config_parameters)
-  serviceConfigString = buildServiceConfigs(config_parameters)
-  config_filepath = "/etc/nagios/conf.d/#{config_parameters['hostname']}.cfg"
-  unless File.exist?("#{config_filepath}")
+  host_config_string = build_host_config(config_parameters)
+  service_config_string = build_service_configs(config_parameters)
+  config_filepath = "#{CONFIG_LOCATION}#{config_parameters['hostname']}.cfg"
+  if File.exist?(config_filepath)
+    status 409
+    Mylog.log.info "config file #{config_filepath} already exist, returning 409"
+  else
     begin
-      config_file = File.new("#{config_filepath}", "w")
-      config_file.puts(serviceConfigString)
-      config_file.puts(hostConfigString)
+      config_file = File.new(config_filepath, 'w')
+      config_file.puts(service_config_string)
+      config_file.puts(host_config_string)
       config_file.close
     rescue => e
-      logger.error "error creating file, received exception #{e.message} return 500"
       status 500
+      Mylog.log.error "error creating file, received exception #{e.message} return 500"
     end
-    if checkConfig() == 0
-      logger.info "config check passed, restarting nagios services"
-      if restartNagios() == 0
-        logger.info "restart of nagios successful return 200"
+    if check_config == 0
+      Mylog.log.info 'config check passed, restarting nagios services'
+      if restart_nagios == 0
         status 200
+        Mylog.log.info 'restart of nagios successful return 200'
       else
-        logger.error "restart of nagios failed return 500"
         status 500
+        Mylog.log.error 'restart of nagios failed return 500'
       end
     else
-      logger.error "config check failed, did not restart nagios services return 400"
       status 400
-      File.delete("#{config_filepath}")
+      Mylog.log.error 'config check failed, did not restart nagios services return 400'
+      File.delete(config_filepath)
     end
-  else
-    status 409
-    logger.info "config file #{config_filepath} already exist, returning 409"
   end
 end
